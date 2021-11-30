02b5e958ed37b2cefd2ba36bf2f2c572339740dd
Gaurang Khedekar <khedekar5758@gmail.com>
2021-11-30 10:11:09 +0530

    Added file from Machine

---
+++ MainLogic.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+    class MainLogic
+    {
+        public WorkItemLinkTypeEnd ForwardEnd { get; set; }
+        StringBuilder sbr = new StringBuilder();
+
+        public void mainMethodd()
+        {
+            #region guk1
+            //    // [DllImport("Microsoft.WITDataStore32.dll", CharSet = CharSet.Unicode)]
+            //    string bugnumber = Console.ReadLine();
+
+            //    TfsTeamProjectCollection tpc= new TfsTeamProjectCollection(new Uri("http://alm-prod-app1:8080/tfs/boc_projects"));
+
+            //    tpc.EnsureAuthenticated();
+            //    WorkItemStore item = (WorkItemStore)tpc.GetService(typeof(WorkItemStore));
+
+            //WorkItem number = item.GetWorkItem(Int32.Parse(bugnumber));
+
+            //    //linkeite
+            //   // WorkItemType wiType = item.Projects[0].WorkItemTypes[10];
+            //  // WorkItemLinkTypeEnd type= (WorkItemLinkTypeEnd)item.Projects[0].WorkItemTypes[10];
+            //    WorkItemLinkTypeEnd linkTyp=item.WorkItemLinkTypes.LinkTypeEnds[wiTyp]
+            #endregion
+
+            #region guk2
+            
+            using (var projectCollection = TfsTeamProjectCollectionFactory.
+            GetTeamProjectCollection(new Uri("http://alm-prod-app1:8080/tfs/boc_projects")))
+            {
+                // get the work item store for the collection
+                var workItemStore = projectCollection.GetService<WorkItemStore>();
+
+                // get the link type for hierarchical relationships
+                var linkType = workItemStore.WorkItemLinkTypes[CoreLinkTypeReferenceNames.Hierarchy];
+                //start reading the file
+                // int[] parentNumbers=readTextFile();
+
+                string[] fileContent = readFileContent();
+                // fetch the work items to be linked
+                //var parentNumber = getParentNumber();
+
+                //*************
+
+                // this is one more time new code
+                for (int i = 0; i < fileContent.Length; i++)
+                {
+                    if (fileContent[i].Split('#')[0] == "") break;
+                    var parentWorkItem = workItemStore.GetWorkItem(Convert.ToInt32(fileContent[i].Split('#')[0]));
+                    var childWorkItem = workItemStore.GetWorkItem(createChild(parentWorkItem,fileContent[i]));
+                    parentWorkItem.Links.Add(new WorkItemLink(linkType.ForwardEnd, childWorkItem.Id));
+                    parentWorkItem.Save();
+                }
+                //this is new code
+
+                //for (int i = 0; i < parentNumbers.Length; i++)
+                //{
+                //    var parentWorkItem = workItemStore.GetWorkItem(parentNumbers[i]);
+                //    var childWorkItem = workItemStore.GetWorkItem(createChild(parentWorkItem));
+                //    parentWorkItem.Links.Add(new WorkItemLink(linkType.ForwardEnd, childWorkItem.Id));
+                //    parentWorkItem.Save();
+                //}
+                //*************
+              //  var parentWorkItem = workItemStore.GetWorkItem(1226648);  //parent
+              
+                //var childWorkItem = workItemStore.GetWorkItem(createChild(parentWorkItem)); // child no
+
+                // add a new link to the parent relating the child and save it
+               // parentWorkItem.Links.Add(new WorkItemLink(linkType.ForwardEnd, childWorkItem.Id));
+                //parentWorkItem.Save();
+                #endregion
+            }
+        }
+
+        //demo method for real method
+        public int createChild(WorkItem parentWorkItem)
+        {
+            WorkItemType wiType = parentWorkItem.Project.WorkItemTypes["Release Note"];
+            WorkItem newWI = new WorkItem(wiType);
+            newWI.Title = parentWorkItem.Title.ToString();   //titel same as parent bug
+            newWI.State = "Draft";
+            //newWI.Fields["Release Note Header"].Value = "guk creating this rnote";  // header needs to be intelligent
+           // StringBuilder Rnote = getRnote();
+            newWI.Fields["Release Note"].Value = sbr.ToString();
+            newWI.Fields["Iteration Path"].Value = "SPFM\\PFM\\Sustaining";
+           // newWI.Fields["Type"].Value = "Resolved Issue";
+            newWI.Fields["Release Note Audience"].Value = "Both";
+            newWI.Fields["Release Note Type"].Value = "Resolved Issue";
+            newWI.Save();
+            
+
+            return newWI.Id;
+        }
+
+        //actual method
+        public int createChild(WorkItem parentWorkItem,string Rlnote)
+        {
+            WorkItemType wiType = parentWorkItem.Project.WorkItemTypes["Release Note"];
+            WorkItem newWI = new WorkItem(wiType);
+            newWI.Title = parentWorkItem.Title.ToString();   //titel same as parent bug
+            newWI.State = "New";
+            //newWI.Fields["Release Note Header"].Value = "guk creating this rnote";  // header needs to be intelligent
+            StringBuilder Rnote = getRnote(Rlnote);
+            newWI.Fields["Release Note"].Value = sbr.ToString();
+            newWI.Fields["Iteration Path"].Value = "SPFM\\PFM\\Sustaining";
+            // newWI.Fields["Type"].Value = "Resolved Issue";
+            newWI.Fields["Release Note Audience"].Value = "Both";
+            newWI.Fields["Release Note Type"].Value = "Resolved Issue";
+            newWI.Save();
+
+
+            return newWI.Id;
+        }
+        //actual method
+        public StringBuilder getRnote(string str)
+        {
+            sbr.Clear();
+            for (int i = 1; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 1:sbr.Append("Problem Description :-");
+                        break;
+                    case 2:sbr.Append("Fix Description :-");
+                        break;
+                    case 3:sbr.Append("Testing Consideration :-");
+                        break;
+                    default:
+                        break;
+                }
+                sbr.Append(Environment.NewLine);
+                string tempString = str.Split('#')[i];
+                sbr.Append(tempString);
+                sbr.Append(Environment.NewLine);
+                sbr.Append(Environment.NewLine);
+
+
+            }
+            sbr.Append("Clients: ALL CLIENTS");
+            return sbr;
+        }
+        //demo method for actual method
+        public StringBuilder getRnote()
+        {
+            //StringBuilder sbr = new StringBuilder();
+            string[] lines;
+            var list = new List<string>();
+            var fileStream = new FileStream(@"c:\GUK.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+            fileStream.Close();
+            fileStream.Dispose();
+            lines = list.ToArray();
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //  string  tempString = lines[0].Split('#')[i];
+            //    sbr.Append(tempString);
+            //    sbr.Append(Environment.NewLine);
+            //}
+            //sbr.Append("Clients: ALL CLIENTS");
+
+
+
+
+            //return sbr;
+            return null;
+        }
+        public int[] readTextFile()
+        {
+            string[] lines;
+            int[] linesnumber;
+            var listnumbers = new List<int>();
+            var list = new List<string>();
+           
+            var fileStream = new FileStream(@"c:\GUK.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+            fileStream.Close();
+            fileStream.Dispose();
+            lines = list.ToArray();
+
+            // read bug number only from first line
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string tempString = lines[i].Split('#')[0];
+                listnumbers.Add(Convert.ToInt32(tempString));
+
+            }
+            linesnumber = listnumbers.ToArray();
+
+
+
+            //read the release note in sbr object of string builder
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 1; j < 4; j++)
+                {
+                    string tempString = lines[i].Split('#')[j];
+                    sbr.Append(tempString);
+                    sbr.Append(Environment.NewLine);
+                    sbr.Append("Clients: ALL CLIENTS");
+                }
+
+            }
+
+            // read bug number
+
+            //read fix and number
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    string tempString = lines[0].Split('#')[i];
+            //    //linesnumber[]
+            //    sbr.Append(tempString);
+            //    sbr.Append(Environment.NewLine);
+            //}
+            //sbr.Append("Clients: ALL CLIENTS");
+
+
+            //read the fix# and the description and pad all clients at end
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    string tempString = lines[i].Split('#')[j];
+                    if (tempString == "") break;
+                    if (j == 0)
+                    {
+                        listnumbers.Add(Convert.ToInt32(tempString));
+                        break;
+                    }
+
+                    sbr.Append(tempString);
+                    sbr.Append(Environment.NewLine);
+                    sbr.Append("Clients: ALL CLIENTS");
+                }
+                
+            }
+            linesnumber = listnumbers.ToArray();
+            return linesnumber;
+        }
+        
+
+
+        //final reading method
+        public string[] readFileContent()             // this should be final reading thing
+        {
+            string[] lines;
+            var listOfLines = new List<string>();
+           
+                OpenFileDialog ObjOpen = new OpenFileDialog();
+                ObjOpen.ShowDialog();
+            // ObjOpen.OpenFile();
+            var fileStream = ObjOpen.OpenFile();
+            //var filename
+            // var fileStream = new FileStream(@"c:\GUK.txt", FileMode.Open, FileAccess.Read);
+            //var fileStream = new FileStream(ObjOpen.OpenFile().ToString(),FileMode.Open);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                   listOfLines.Add(line);
+                }
+            }
+            fileStream.Close();
+            fileStream.Dispose();
+            lines = listOfLines.ToArray();
+            return lines; 
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Net;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        [DllImport("Microsoft.WITDataStore32.dll", CharSet = CharSet.Unicode)]
+        public static extern int hello();
+        [STAThread]
+        static void Main(string[] args)
+        {
+            #region classiccode
+            // THIS IS CLASSIC ******************************************
+            MainLogic mlObj = new MainLogic();
+            mlObj.mainMethodd();
+            
+            // *********************************
+
+            #endregion
+            #region trial3
+            //int[] numbers = new int[15];
+            //numbers[0] = 2571664;
+            //numbers[1] = 2814825;
+            //numbers[2] = 2872067;
+            //numbers[3] = 2849622;
+            //numbers[4] = 2316527;
+            //numbers[5] = 3111967;
+            //numbers[6] = 3115163;
+            //numbers[7] = 3129909;
+            //numbers[8] = 3147021;
+            //numbers[9] = 3188105;
+            //numbers[10] = 3180864;
+            //numbers[11] = 3199403;
+            //numbers[12] = 3224017;
+            //numbers[13] = 2809057;
+            //numbers[14] = 3377269;
+
+            //TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(new Uri("http://alm-prod-app1:8080/tfs/boc_projects"));
+            //tpc.EnsureAuthenticated();
+            //WorkItemStore item = (WorkItemStore)tpc.GetService(typeof(WorkItemStore));
+            //TfsConfigurationServer config = TfsConfigurationServerFactory.GetConfigurationServer(new Uri("http://alm-prod-app1:8080/tfs/boc_projects"));
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    WorkItem WInumber = item.GetWorkItem(numbers[i]);
+            //    Console.WriteLine(numbers[i]+"  =  "+WInumber.IterationPath);
+
+            //    Console.WriteLine("*****************************");
+
+            //}
+
+
+
+            #endregion
+            //     [DllImport("Microsoft.WITDataStore32.dll", CharSet = CharSet.Unicode)]
+            #region trail2
+            ////TeamFoundationServer tfs = TeamFoundationServerFactory.GetServer("TFSATServer");
+            //string bugnumber = Console.ReadLine();
+
+            //// WindowsCredential wincred = new WindowsCredential(cred);
+
+            //TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(new Uri("http://alm-prod-app1:8080/tfs/boc_projects"));
+            //tpc.EnsureAuthenticated();
+            //WorkItemStore item = (WorkItemStore)tpc.GetService(typeof(WorkItemStore));
+            //TfsConfigurationServer config = TfsConfigurationServerFactory.GetConfigurationServer(new Uri("http://alm-prod-app1:8080/tfs/boc_projects"));
+            //WorkItem number = item.GetWorkItem(Int32.Parse(bugnumber));
+
+            //Console.WriteLine(number.AreaPath);
+            //Console.WriteLine(number.Title.ToString());
+            //// Console.WriteLine(number.Fields["Target Release"].Value.ToString());
+            ////Console.Read();
+            ////creating new thing
+            //for (int i = 0; i < number.Fields.Count; i++)
+            //{
+            //    if (number.Fields[i].Value != null) Console.WriteLine((number.Fields[i].Name + "=" + number.Fields[i].Value.ToString()));
+            //    Console.WriteLine();
+            //}
+            ////get the parent
+            ////var parentLink=number.WorkItemLinks.Cast<WorkItem>().FirstOrDefault()
+            
+            //WorkItem parentitem = getParentWorkItemMethod(number, item);
+
+
+            ////get the parent ends here
+
+            ////Project teampro = item.Projects["SPFM"];
+            //TeamProjectPicker pp = new TeamProjectPicker(TeamProjectPickerMode.SingleProject, false);
+            //pp.ShowDialog();
+            //pp.SelectedTeamProjectCollection.EnsureAuthenticated();
+            //WorkItemType wiType = item.Projects[0].WorkItemTypes[10];
+
+            //WorkItem newWI = new WorkItem(wiType);
+
+            //newWI.Title = "Title by GUK";
+            //newWI.State = "New";
+            ////newWI.IterationPath = "SPFM\\PFM\\17.3\\I19";
+            //// newWI.Fields["Team"].Value = "PFM-Fighting Bees";
+            //newWI.Fields["Release Note Header"].Value = "guk creating this rnote";
+            //newWI.Fields["Release Note"].Value = "thisadoa dfadsf asdfasd";
+            //newWI.Fields["Work Item Type"].Value = wiType.Name;
+            //newWI.Fields["Area Path"].Value = pp.SelectedTeamProjectCollection.Name.ToString();
+            //// newWI.Fields["Area Id"].Value = pp.SelectedTeamProjectCollection.InstanceId.ToString();
+            ////pp.SelectedProjects.
+            //var a = pp.SelectedProjects;     //information of selected proect
+            //foreach (var property in a)
+            //{
+            //    Console.WriteLine(property.ToString());
+            //}
+            ////newWI.Fields["Assigned To"].Value = "Khedekar,Gaurang";
+            ////newWI.Fields["Team Project"].Value = "SPFM";
+            ////newWI.Fields["Area Path"].Value = "SPFM\\PFM\\Regulatory (Optum)\\State Reporting";
+
+            ////newWI.Fields["System.AssignedTo"].Value = "User1";
+            //ArrayList aListval = newWI.Validate();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("********************************************************************************");
+            //for (int i = 0; i < newWI.Fields.Count; i++)
+            //{
+            //    if (newWI.Fields[i].Value != null) Console.WriteLine((newWI.Fields[i].Name + "=" + newWI.Fields[i].Value.ToString()));
+            //    Console.WriteLine();
+            //}
+
+            //// now this is giving error becuase I am landing in first project not SPFM
+            //newWI.Save();
+
+            //Console.WriteLine(newWI.Id);
+
+            ////Console.WriteLine(ex.Message); 
+            //Console.WriteLine("SAVED SUCCEFUKLLY");
+            //Console.Read();
+            #endregion
+
+            #region trial1
+            //Uri collectionUri = (args.Length < 1) ?
+            //        new Uri("http://alm-prod-app1:8080/tfs/boc_projects") : new Uri(args[0]);
+            //    TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(collectionUri);
+            //    WorkItemStore workItemStore = tpc.GetService<WorkItemStore>();
+            //    Project teamProject = workItemStore.Projects["DinnerNow"];
+            //   // WorkItemType workItemType = teamProject.WorkItemTypes["User Story"];
+            //    WorkItemType WT = teamProject.WorkItemTypes["Release Note"];
+
+            //    // Create the work item. 
+            //    WorkItem userStory = new WorkItem(WT)
+            //    {
+            //        // The title is generally the only required field that doesn’t have a default value. 
+            //        // You must set it, or you can’t save the work item. If you’re working with another
+            //        // type of work item, there may be other fields that you’ll have to set.
+            //        Title = "Recently ordered menu",
+            //        Description =
+            //            "As a return customer, I want to see items that I've recently ordered."
+            //    };
+
+            //    // Save the new user story. 
+            //    userStory.Save();
+            #endregion
+        }
+
+        static WorkItem getParentWorkItemMethod(WorkItem id,WorkItemStore k)
+        {
+            StringBuilder queryString = new StringBuilder("SELECT [System.Id]" +
+                                                  " FROM WorkItemLinks " +
+                                                  " WHERE [Source].[System.WorkItemType] = '" + id.Fields["Work Item Type"].Value.ToString()+ "'" +
+                                                  //" AND [Source].[System.TeamProject] = '" + TFS_TIMESHEET_PROJECT_KEY + "'" +
+                                                  " AND [Source].[System.Id] = " + Convert.ToInt32(          id.Fields["ID"].Value)
+                                                  );
+            Query wiQuery = new Query(k, queryString.ToString());
+           // Query worquery=new Query()
+            WorkItemLinkInfo[] wiTrees = wiQuery.RunLinkQuery();
+            WorkItem wi = k.GetWorkItem(wiTrees[1].TargetId);
+
+            return wi;
+            //return null;
+        }
+    }
+}
